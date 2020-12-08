ecd2dc324c6aae3657b15225d772396d251936a3
Shereef Bishay <shereef@gmail.com>
2020-12-08 00:17:10 -0800

    google analytics

--- public/js/main.js
+++ public/js/main.js
@@ -28,14 +28,6 @@ var isMobile = {
 
 $(document).ready(function () {
 
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-        
-        ga('create', 'G-8ZQPV59TFS', 'auto');
-        ga('send', 'pageview');
-
     bind_shrink_header();
 
       // Create an instance of the Stripe object with your publishable API key
